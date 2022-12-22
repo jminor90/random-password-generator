9486b46e6c95c7aeaef289b077e872f88b12f37e
Joshua <minor.jbm@gmail.com>
2022-12-21 18:45:52 -0700

    Script able to confirm information, but still need to actually generate password...

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,6 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+let userResponse;
+
+function generatePassword() {
+  //prompt user
+  let passLength = prompt("Length?")
+  if (passLength < 8 || passLength >128) {
+    passAlert = alert("Password must be between 8 and 128 characterss")
+    console.log("WRONG")
+    generatePassword();
+  } else {
+    passLower = confirm("Would you like to include lower case?")
+    passUpper = confirm("Would you like to include upper case?")
+    passNumeric = confirm("Would you like to include numerals?")
+    console.log(passLower)
+    console.log(passUpper)
+    console.log(passNumeric)
+  }
+  //length 8-128
+  //lowercase?
+  //uppercase?
+  //numeric?
+  //special characters?
+  //validated and at least ONE character type selected
+  //password is generated
+  //password displayed
+
+  //console.log("yo");
+  return password;
+};
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +40,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
