423f4b14e3e4acc7a48d80910a3a86cf17dd1258
Joshua <minor.jbm@gmail.com>
2022-12-21 19:49:49 -0700

    Getting closer...password generates number or text at the correct length but no randomization still...

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,23 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const numerals = [0,1,2,3,4,5,6,7,8,9]
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+
 
 let userResponse;
+let availCharmander;
+let password = ''
 
 function generatePassword() {
+
   //prompt user
   let passLength = prompt("Length?")
   if (passLength < 8 || passLength >128) {
     passAlert = alert("Password must be between 8 and 128 characterss")
     console.log("WRONG")
-    generatePassword();
+
   } else {
-    passLower = confirm("Would you like to include lower case?")
-    passUpper = confirm("Would you like to include upper case?")
     passNumeric = confirm("Would you like to include numerals?")
-    console.log(passLower)
-    console.log(passUpper)
-    console.log(passNumeric)
+    //passLower = confirm("Would you like to include lower case?")
+    //passUpper = confirm("Would you like to include upper case?")
+    
+
+    //console.log(passLower)
+    //console.log(passUpper)
+
+
+
   }
+  console.log(passLength)
+  console.log(passNumeric)
+  
+  if (passNumeric === true) {
+    availCharmander = numerals
+  } else
+  availCharmander = upperCase
+
+
+
+  for (i=0; i < passLength; i += 1) {
+    const randomIndex = Math.floor(Math.random() *passLength.length)
+    password += availCharmander[randomIndex];
+
+  } console.log(password)
+
+
   //length 8-128
   //lowercase?
   //uppercase?
